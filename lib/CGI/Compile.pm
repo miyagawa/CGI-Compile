miyagawa/CGI-Compile

commit 82f26fc0e4140bea18f25ad3602150e01f751c6f

Use better package/sub names for CGI coderefs.
Fix #8.

Use path of CGI script as identifier-safe package name, and the filename
as the identifier-safe coderef name.

This replaces the previous behavior of using the full path as the
package name for an anon coderef.

Replace the private method:

$package = $self->_build_package($path);

with

($package, $subname) = $self->_build_subname($path);

and update the documentation.

For scalar refs, use the caller's package with a counter per-package, to
form names of the form __CGI0__, __CGI1__ etc.. Instead of regular
anonymous coderefs in CGI::Compile.

Add t/symbols.t to test the assigned package and coderef names in both
cases.

Add main dependency on Sub::Name and a test dependency on Sub::Identify.

Signed-off-by: Rafael Kitover <rkitover@gmail.com>

lib/CGI/Compile.pm
@@ -12,6 +12,7 @@ use File::pushd;
 use File::Temp;
 use File::Spec;
 use File::Path;
+use Sub::Name 'subname';
 
 our $RETURN_EXIT_VAL = undef;
 
@@ -53,21 +54,33 @@ BEGIN {
     die $@ if $@;
 }
 
+my %anon;
+
 sub compile {
     my($class, $script, $package) = @_;
 
     my $self = ref $class ? $class : $class->new;
 
-    my($code, $path, $dir);
+    my($code, $path, $dir, $subname);
+
     if (ref $script eq 'SCALAR') {
-        $code = $$script;
+        $code      = $$script;
+
+        $package ||= (caller)[0];
+
+        $subname   = '__CGI' . $anon{$package}++ . '__';
     } else {
         $code = $self->_read_source($script);
+
         $path = Cwd::abs_path($script);
         $dir  = File::Basename::dirname($path);
-    }
 
-    $package ||= $self->_build_package($path || $script);
+        my $genned_package;
+
+        ($genned_package, $subname) = $self->_build_subname($path || $script);
+
+        $package ||= $genned_package;
+    }
 
     my $warnings = $code =~ /^#!.*\s-w\b/ ? 1 : 0;
     $code =~ s/^__END__\r?\n.*//ms;
@@ -77,7 +90,7 @@ sub compile {
     # TODO handle nph and command line switches?
     my $eval = join '',
         "package $package;",
-        "sub {",
+        'sub {',
         'local $CGI::Compile::USE_REAL_EXIT = 0;',
         "\nCGI::initialize_globals() if defined &CGI::initialize_globals;",
         'local ($0, $CGI::Compile::_dir, *DATA);',
@@ -119,7 +132,6 @@ sub compile {
         },
         '};';
 
-
     my $sub = do {
         no warnings 'uninitialized'; # for 5.8
         # NOTE: this is a workaround to fix a problem in Perl 5.10
@@ -131,7 +143,7 @@ sub compile {
 
         die "Could not compile $script: $exception" if $exception;
 
-        sub {
+        subname "${package}::$subname", sub {
             my @args = @_;
             # this is necessary for MSWin32
             my $orig_warn = $SIG{__WARN__} || sub { warn(@_) };
@@ -150,21 +162,24 @@ sub _read_source {
     return do { local $/; <$fh> };
 }
 
-sub _build_package {
+sub _build_subname {
     my($self, $path) = @_;
 
     my ($volume, $dirs, $file) = File::Spec::Functions::splitpath($path);
     my @dirs = File::Spec::Functions::splitdir($dirs);
-    my $package = join '_', grep { defined && length } $volume, @dirs, $file;
+
+    my $package = join '_', grep { defined && length } $volume, @dirs;
+    my $name    = $file;
 
     # Escape everything into valid perl identifiers
-    $package =~ s/([^A-Za-z0-9_])/sprintf("_%2x", unpack("C", $1))/eg;
+    s/([^A-Za-z0-9_])/sprintf("_%2x", unpack("C", $1))/eg for $package, $name;
 
-    # make sure that the sub-package doesn't start with a digit
-    $package =~ s/^(\d)/_$1/;
+    # make sure the identifiers don't start with a digit
+    s/^(\d)/_$1/ for $package, $name;
 
-    $package = $self->{namespace_root} . "::$package";
-    return $package;
+    $package = $self->{namespace_root} . ($package ? "::$package" : '');
+
+    return ($package, $name);
 }
 
 # define tmp_dir value later on first usage, otherwise all children
@@ -446,10 +461,11 @@ The contents of the file as a scalar string.
 
 =back
 
-=head2 _build_package
+=head2 _build_subname
 
-Creates a package name into which the CGI coderef will be compiled into,
-prepended with C<$self->{namespace_root}>.
+Creates a package name and coderef name into which the CGI coderef will be
+compiled into. The package name will be prepended with
+C<$self->{namespace_root}>.
 
 Parameters:
 
@@ -471,6 +487,15 @@ Returns:
 The generated package name.
 
 =back
+
+=over 4
+
+=item * C<$subname>
+
+The generated coderef name, based on the file name (without directory) of the
+CGI file path.
+
+=over 4
 
 =head2 _eval
 

t/symbols.t
@@ -1,0 +1,43 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+use CGI::Compile;
+use Capture::Tiny 'capture_stdout';
+use Test::Requires 'Sub::Identify';
+
+use Sub::Identify qw/sub_name stash_name/;
+
+my $cgi =<<'EOL';
+#!/usr/bin/perl
+
+print "Content-Type: text/plain\015\012\015\012", <DATA>;
+
+__DATA__
+Hello
+World
+EOL
+
+my $sub = CGI::Compile->compile(\$cgi);
+
+is sub_name($sub),   '__CGI0__';
+is stash_name($sub), 'main';
+
+$sub = CGI::Compile->compile('t/hello.cgi');
+
+is sub_name($sub), 'hello_2ecgi';
+
+like stash_name($sub), qr/^CGI::Compile::ROOT::[A-Za-z0-9_]*t\z/;
+
+$sub = CGI::Compile->compile(\$cgi);
+
+is sub_name($sub),   '__CGI1__';
+is stash_name($sub), 'main';
+
+my $out = capture_stdout { $sub->() };
+
+like $out, qr/Hello\nWorld/;
+
+done_testing;
+

cpanfile
@@ -1,5 +1,6 @@
 requires 'perl', '5.008001';
 requires 'File::pushd';
+requires 'Sub::Name';
 
 on configure => sub {
     requires 'Module::Build::Tiny';
@@ -13,6 +14,7 @@ on test => sub {
     requires 'Try::Tiny';
     requires 'CGI';
     requires 'Switch';
+    requires 'Sub::Identify';
 };
 
 on develop => sub {

t/source.t
@@ -1,9 +1,12 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
 use Test::More;
 use CGI::Compile;
 use Capture::Tiny 'capture_stdout';
 
-{
-    my $str =<<EOL;
+my $cgi =<<'EOL';
 #!/usr/bin/perl
 
 print "Content-Type: text/plain\015\012\015\012", <DATA>;
@@ -13,10 +16,11 @@ Hello
 World
 EOL
 
-    my $sub = CGI::Compile->compile(\$str);
-    my $out = capture_stdout { $sub->() };
-    like $out, qr/Hello\nWorld/;
-}
+my $sub = CGI::Compile->compile(\$cgi);
+
+my $out = capture_stdout { $sub->() };
+
+like $out, qr/Hello\nWorld/;
 
 done_testing;
 

commit 1ee17c581bb866dcd79bdf8579d3242176037a39

Add file name to CGI coderef package name.
Fix regression introduced in 82f26fc, that caused all CGIs in the same
directory to be compiled into the same package, causing weird behavior.

- Fix #25.

Signed-off-by: Rafael Kitover <rkitover@gmail.com>

lib/CGI/Compile.pm
@@ -168,8 +168,8 @@ sub _build_subname {
     my ($volume, $dirs, $file) = File::Spec::Functions::splitpath($path);
     my @dirs = File::Spec::Functions::splitdir($dirs);
 
-    my $package = join '_', grep { defined && length } $volume, @dirs;
     my $name    = $file;
+    my $package = join '_', grep { defined && length } $volume, @dirs, $name;
 
     # Escape everything into valid perl identifiers
     s/([^A-Za-z0-9_])/sprintf("_%2x", unpack("C", $1))/eg for $package, $name;

t/symbols.t
@@ -28,7 +28,7 @@ $sub = CGI::Compile->compile('t/hello.cgi');
 
 is sub_name($sub), 'hello_2ecgi';
 
-like stash_name($sub), qr/^CGI::Compile::ROOT::[A-Za-z0-9_]*t\z/;
+like stash_name($sub), qr/^CGI::Compile::ROOT::[A-Za-z0-9_]*t_hello_2ecgi\z/;
 
 $sub = CGI::Compile->compile(\$cgi);
 